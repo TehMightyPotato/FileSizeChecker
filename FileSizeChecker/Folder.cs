6f07cedfb408a888c7c764e60b2cebd337dbd248
Nicolas Teichtweier <contact@mightypotato.de>
2020-02-10 01:24:40 +0100

    added failsafe for folders and files without readpermission

--- FileSizeChecker/Folder.cs
+++ FileSizeChecker/Folder.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
+using System.Text;
 
 namespace FileSizeChecker
 {
@@ -13,49 +13,82 @@ namespace FileSizeChecker
 
         private DirectoryInfo directoryInfo;
 
-        public Folder(string path)
+        public Folder (string path)
         {
             this.path = path;
-            directoryInfo = new DirectoryInfo(path);
-            files = new List<File>();
-            folders = new List<Folder>();
+            directoryInfo = new DirectoryInfo (path);
+            files = new List<File> ();
+            folders = new List<Folder> ();
         }
 
-        public void AddFolder(Folder folder)
+        public void AddFolder (Folder folder)
         {
-            folders.Add(folder);
+            folders.Add (folder);
         }
 
-        public void GetFiles()
+        public void GetFiles ()
         {
-            var files = directoryInfo.GetFiles();
-            foreach(var file in files)
+            if (Program.verbose)
             {
-                this.files.Add(new File(file.DirectoryName, file.Name, file.Length));
+                Console.WriteLine ("Getting file info of folder: {0}...", path);
+            }
+            try
+            {
+                var files = directoryInfo.GetFiles ();
+                foreach (var file in files)
+                {
+                    this.files.Add (new File (file.DirectoryName, file.Name, file.Length));
 
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                if (Program.verbose)
+                {
+                    Console.WriteLine ("Could not access files at: {0}  PERMISSION DENIED", path);
+                }
+            }
+            if (Program.verbose)
+            {
+                Console.WriteLine ("Done!");
             }
         }
 
-        public void GetFolders()
+        public void GetFolders ()
         {
-            var folders = directoryInfo.GetDirectories();
-            foreach(var folder in folders)
+            if (Program.verbose)
             {
-                this.folders.Add(new Folder(folder.FullName));
+                Console.WriteLine ("Getting subfolder list of folder: {0}...", path);
+            }
+
+            try
+            {
+                var folders = directoryInfo.GetDirectories ();
+                foreach (var folder in folders)
+                {
+                    this.folders.Add (new Folder (folder.FullName));
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                if (Program.verbose)
+                {
+                    Console.WriteLine ("Could not access folder at: {0}  PERMISSION DENIED", path);
+                }
+            }
+            if (Program.verbose)
+            {
+                Console.WriteLine ("Done!");
             }
         }
 
-        public void Execute()
+        public void Execute ()
         {
-            Console.WriteLine("Getting file info of folder: {0}...", path);
-            GetFiles();
-            Console.WriteLine("Done!");
-            Console.WriteLine("Getting subfolder list of folder: {0}...", path);
-            GetFolders();
-            Console.WriteLine("Done!");
-            foreach(var folder in folders)
+            GetFiles ();
+            GetFolders ();
+            foreach (var folder in folders)
             {
-                folder.Execute();
+                folder.Execute ();
             }
         }
     }
--- FileSizeChecker/Program.cs
+++ FileSizeChecker/Program.cs
@@ -1,41 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Diagnostics;
 
 namespace FileSizeChecker
 {
     class Program
     {
+        public static bool verbose;
+
         public static long maxFileSize;
         public static List<File> filesTooBig;
 
-        static void Main(string[] args)
+        /// <summary>
+        /// A tool to check a folder and its subfolders for files bigger than a defined maximum.
+        /// </summary>
+        /// <param name="verbose">Verbosity Parameter. Use this if you want more info on which folder is being scanned. Default: false</param>
+        /// <param name="maxFileSize">Set a maximum file size in MB. Default: 100MB</param>
+        static void Main (bool verbose = false, long maxFileSize = 100)
         {
-            filesTooBig = new List<File>();
-            maxFileSize = 100000000;
-            var root = new Folder(Environment.CurrentDirectory);
-            root.Execute();
-            if(filesTooBig.Count > 0)
+            Program.verbose = verbose;
+            Program.maxFileSize = maxFileSize * 1000000;
+
+            filesTooBig = new List<File> ();
+            var root = new Folder (Environment.CurrentDirectory);
+            var stopwatch = Stopwatch.StartNew ();
+            root.Execute ();
+            if (filesTooBig.Count > 0)
             {
-                PrintFilesTooBig();
+                PrintFilesTooBig ();
             }
             else
             {
-                Console.WriteLine("No files bigger than {0} bytes found!", maxFileSize);
+                Console.WriteLine ("No files bigger than {0} MB found!", Program.maxFileSize / 1000000);
             }
+            stopwatch.Stop ();
+            Console.WriteLine ("Elapsed Time: {0}", stopwatch.Elapsed);
         }
 
-        public static void FileTooBig(File file)
+        public static void FileTooBig (File file)
         {
-            filesTooBig.Add(file);
+            filesTooBig.Add (file);
         }
 
-        static void PrintFilesTooBig()
+        static void PrintFilesTooBig ()
         {
-            Console.WriteLine("Files that exceeded the {0} byte file size limit: ", maxFileSize);
-            foreach(var file in filesTooBig)
+            Console.WriteLine ("Files that exceeded the {0} byte file size limit: ", maxFileSize);
+            foreach (var file in filesTooBig)
             {
-                Console.WriteLine("Name: {0}, Path: {1}, Size: {2}", file.name, file.path, file.size);
+                Console.WriteLine ("Name: {0}, Path: {1}, Size: {2}", file.name, file.path, file.size);
             }
         }
     }
